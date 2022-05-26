5691937a0080386ff59cda875404eb01076525c1
vgishyan <vahagn.gishyan.a@gmail.com>
2022-05-26 10:30:36 +0400

    Masters::Graph::Matrix::Edge, update

--- Sources/Graph/Matrix/Edge.hpp
+++ Sources/Graph/Matrix/Edge.hpp
@@ -34,20 +34,59 @@ namespace Masters
 				using CWeight  = Edge::CWeight;
 
 			public: // ctor/dector
-				CEdge(const CLocation node, const CWeight weight);
+				CEdge(const CLocation& node,  const CWeight&  weight);
+				CEdge(      CLocation&& node,       CWeight&& weight);
 				virtual ~CEdge();
 
 			public: // get set
 				const CLocation node() const noexcept;
 				const CWeight   weight() const noexcept;
 
-				void node  (const CLocation nodeIndex)  noexcept;
-				void weight(const CWeight   edgeWeight) noexcept;
+				void node  (const CLocation&  nodeIndex)  noexcept;
+				void node  (      CLocation&& nodeIndex)  noexcept;
+				void weight(const CWeight&    edgeWeight) noexcept;
+				void weight(      CWeight&&   edgeWeight) noexcept;
 
 			private: // member vars
 				//CLocation	m_root; // from
 				CWeight   m_weight; // ????
 				CLocation m_node;   // to
+			};
+
+			class CFullEdgeRef
+			{
+
+			};
+
+			class CFullEdge 
+			{
+			private: // typedefs
+				using CWeight = Edge::CWeight;
+
+			public: // ctor/dector
+				CFullEdge(const CLocation&  m_root, const CLocation&  node, const CWeight&  weight);
+				CFullEdge(      CLocation&& m_root,       CLocation&& node,       CWeight&& weight);
+				
+				CFullEdge(const CLocation&  m_root, const CEdge&  edge);
+				CFullEdge(      CLocation&& m_root,       CEdge&& edge);
+				
+				virtual ~CFullEdge();
+
+			public: // get set
+				const CLocation root() const noexcept;
+				const CLocation node() const noexcept;
+				const CWeight   weight() const noexcept;
+
+				void root  (const CLocation&  nodeIndex)  noexcept;
+				void root  (      CLocation&& nodeIndex)  noexcept;
+				void node  (const CLocation&  nodeIndex)  noexcept;
+				void node  (      CLocation&& nodeIndex)  noexcept;
+				void weight(const CWeight&    edgeWeight) noexcept;
+				void weight(      CWeight&&   edgeWeight) noexcept;
+
+			private: // member vars
+				CLocation m_root;   // from
+				CEdge     m_edge;
 			};
 
 			using CEdgeList = std::vector<CEdge>;
--- Sources/Graph/Matrix/Srcs/Edge.cpp
+++ Sources/Graph/Matrix/Srcs/Edge.cpp
@@ -10,16 +10,45 @@ namespace Masters
 	{
 		namespace Matrix
 		{
-			CEdge:: CEdge(const CLocation node, const CWeight weight) : m_node(node), m_weight(weight) {}
+			CEdge:: CEdge(const CLocation&  node, const CWeight&  weight) : m_node(node), m_weight(weight) {}
+			CEdge:: CEdge(      CLocation&& node,       CWeight&& weight) : m_node(node), m_weight(weight) {}
 			CEdge::~CEdge() {}
+
+			////////////////////////////////////////////////////////////////////////////////////////////////////
+
+			CFullEdge::CFullEdge(const CLocation&  root, const CLocation&  node, const CWeight&  weight) 
+				: m_root(root), m_edge(node, weight) {}
+			CFullEdge::CFullEdge(      CLocation&& root,       CLocation&& node,       CWeight&& weight)
+				: m_root(root), m_edge(node, weight) {}
+			CFullEdge::CFullEdge(const CLocation&  root, const CEdge&  edge) 
+				: m_root(root), m_edge(edge)         {}
+			CFullEdge::CFullEdge(      CLocation&& root,       CEdge&& edge)
+				: m_root(root), m_edge(edge) {}
+
+			CFullEdge::~CFullEdge() {}
 
 			////////////////////////////////////////////////////////////////////////////////////////////////////
 			
 			const CLocation     CEdge::node  () const noexcept { return (m_node);   }
 			const Edge::CWeight CEdge::weight() const noexcept { return (m_weight); }
 
-			void CEdge::node  (const CLocation node)             noexcept { m_node = node; }
-			void CEdge::weight(const CEdge::CWeight  edgeWeight) noexcept { m_weight = edgeWeight; }
+			void CEdge::node  (const CLocation&       node)       noexcept { m_node = node; }
+			void CEdge::node  (      CLocation&&      node)       noexcept { m_node = node; }
+			void CEdge::weight(const CEdge::CWeight&  edgeWeight) noexcept { m_weight = edgeWeight; }
+			void CEdge::weight(      CEdge::CWeight&& edgeWeight) noexcept { m_weight = edgeWeight; }
+
+			////////////////////////////////////////////////////////////////////////////////////////////////////
+
+			const CLocation     CFullEdge::root()                   const noexcept { return (m_root); }
+			const CLocation     CFullEdge::node()                   const noexcept { return (m_edge.node()); }
+			const Edge::CWeight CFullEdge::weight()                 const noexcept { return (m_edge.weight()); }
+
+			void CFullEdge::root  (const CLocation&           root)       noexcept { m_root = root; }
+			void CFullEdge::root  (      CLocation&&          root)       noexcept { m_root = root; }
+			void CFullEdge::node  (const CLocation&           node)       noexcept { m_edge.node(node); }
+			void CFullEdge::node  (      CLocation&&          node)       noexcept { m_edge.node(node); }
+			void CFullEdge::weight(const CFullEdge::CWeight&  edgeWeight) noexcept { m_edge.weight(edgeWeight); }
+			void CFullEdge::weight(      CFullEdge::CWeight&& edgeWeight) noexcept { m_edge.weight(edgeWeight); }
 
 			////////////////////////////////////////////////////////////////////////////////////////////////////
 
